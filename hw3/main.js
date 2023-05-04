5b040d8eeb769cdf7602dd4a916c391b93696345
Sergiiweb <sergii.web@gmail.com>
2023-05-04 16:19:40 +0300

    hw3

--- hw3/main.js
+++ hw3/main.js
@@ -1,19 +1,19 @@
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
 
-for (let i = 1; i <= 10;  i++) {
+for (let i = 1; i <= 10; i++) {
     document.write(`<div>This is ${i} div</div>`);
 }
 
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
 
-for (let i = 0; i < 10;  i++) {
+for (let i = 0; i < 10; i++) {
     document.write(`<div>This is DIV with ${i} index</div>`);
 }
 
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
 
 let i1 = 1;
-while (i1 <= 20){
+while (i1 <= 20) {
     document.write(`<h1>This is ${i1} H1</h1>`);
     i1++;
 }
@@ -21,7 +21,7 @@ while (i1 <= 20){
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
 
 let i2 = 0;
-while (i2 < 20){
+while (i2 < 20) {
     document.write(`<h1>This is H1 with ${i2} index</h1>`);
     i2++;
 }
@@ -46,9 +46,9 @@ let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'an
 // замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
 
 document.write(`<ul>`);
-    for (const item of listOfItems) {
-        document.write(`<li>${item}</li>`)
-    }
+for (const item of listOfItems) {
+    document.write(`<li>${item}</li>`)
+}
 document.write(`</ul>`);
 
 // -------------------------------------------------------------------------------------------------------------
@@ -90,27 +90,60 @@ let products = [
 for (const product of products) {
     document.write(`<div class="product-card">
                          <h3 class="product-title">${product.title}. Price - ${product.price} UAH</h3>
-                         <img src="${product.image}" alt="" class="product-image">
+                         <img src="${product.image}" alt="${product.title}" class="product-image">
                      </div>    
     `)
 }
 
-// --------------------
+// ------------------------------------------------------------------------------------------------------------
 //     є масив
-// let users = [
-//     {name: 'vasya', age: 31, status: false},
-//     {name: 'petya', age: 30, status: true},
-//     {name: 'kolya', age: 29, status: true},
-//     {name: 'olya', age: 28, status: false},
-//     {name: 'max', age: 30, status: true},
-//     {name: 'anya', age: 31, status: false},
-//     {name: 'oleg', age: 28, status: false},
-//     {name: 'andrey', age: 29, status: true},
-//     {name: 'masha', age: 30, status: true},
-//     {name: 'olya', age: 31, status: false},
-//     {name: 'max', age: 31, status: true}
-// ];
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
 // за допомоги циклу вивести:
-//     - користувачів зі статусом true
+// - користувачів зі статусом true
 // - користувачів зі статусом false
 // - користувачів які старші за 30 років
+
+document.write(`
+        <h3>Користувачі зі статусом true:</h3>
+        <ol>
+    `)
+for (const user of users) {
+    if (user.status === true) {
+        document.write(`<li>Name: ${user.name}. Age: ${user.age}. Status: ${user.status}.</li>`);
+    }
+}
+document.write(`</ol>`);
+
+document.write(`
+        <h3>Користувачі зі статусом false:</h3>
+        <ol>
+    `)
+for (const user of users) {
+    if (user.status === false) {
+        document.write(`<li>Name: ${user.name}. Age: ${user.age}. Status: ${user.status}.</li>`);
+    }
+}
+document.write(`</ol>`);
+
+document.write(`
+        <h3>Користувачі, які старші за 30 років:</h3>
+        <ol>
+    `)
+for (const user of users) {
+    if (user.age > 30) {
+        document.write(`<li>Name: ${user.name}. Age: ${user.age}. Status: ${user.status}.</li>`);
+    }
+}
+document.write(`</ol>`);
