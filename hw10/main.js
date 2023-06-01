3b549cceffebfb4e9aaf74e12ed79565f05d0c30
Sergiiweb <sergii.web@gmail.com>
2023-06-01 11:57:48 +0300

    hw10

--- hw10/main.js
+++ hw10/main.js
@@ -39,10 +39,56 @@ console.log(JSON.parse(localStorage.getItem('sessions')));
 // при завантажені сторінки з'являються перші 10 об'єктів.
 //     При натисканні next виводяться настпні 10 об'єктів
 // При натисканні prev виводяться попередні 10 об'єктів
-//
-//
-//
-//
+const array100 = [];
+for (let i = 1; i < 101; i++) {
+    let obj = {name: `Name${i}`, surname: `Surname${i}`, age: Math.ceil(Math.random() * 100)};
+    array100.push(obj);
+}
+console.log(array100);
+let pagOutput = document.getElementById('pagOutput');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+let j = 10;
+
+for (let i = j - 10; i < j; i++) {
+    let item = document.createElement('div');
+    item.classList.add('pag-item');
+    item.innerText = `Name: ${array100[i].name}, Surname: ${array100[i].surname}, Age: ${array100[i].age}`;
+    pagOutput.appendChild(item);
+    prev.style.visibility = 'hidden';
+}
+next.onclick = function () {
+    j += 10;
+    pagOutput.innerHTML = '';
+    for (let i = j - 10; i < j; i++) {
+        let item = document.createElement('div');
+        item.classList.add('pag-item');
+        item.innerText = `Name: ${array100[i].name}, Surname: ${array100[i].surname}, Age: ${array100[i].age}`;
+        pagOutput.appendChild(item);
+        if (j === 100) {
+            next.style.visibility = 'hidden';
+        }
+        if (j > 10) {
+            prev.style.visibility = 'unset';
+        }
+    }
+}
+prev.onclick = function () {
+    j -= 10;
+    pagOutput.innerHTML = '';
+    for (let i = j - 10; i < j; i++) {
+        let item = document.createElement('div');
+        item.classList.add('pag-item');
+        item.innerText = `Name: ${array100[i].name}, Surname: ${array100[i].surname}, Age: ${array100[i].age}`;
+        pagOutput.appendChild(item);
+        if (j === 10) {
+            prev.style.visibility = 'hidden';
+        }
+        if (j < 100) {
+            next.style.visibility = 'unset';
+        }
+    }
+}
 // - Створити довільний елемент з id = text та створити кнопку.Використовуючи JavaScript, зробіть так,
 // щоб при натисканні на кнопку зникав елемент з id="text".
 //
