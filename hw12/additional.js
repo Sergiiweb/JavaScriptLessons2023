4b16129d9ccf7d16a22c4a9b9dc897d6c540085d
Sergiiweb <sergii.web@gmail.com>
2023-06-08 19:14:23 +0300

    hw12 + additional

--- hw12/additional.js
+++ hw12/additional.js
@@ -9,7 +9,7 @@ let url = new URL('https://jsonplaceholder.typicode.com/users');
 fetch(url)
     .then(response => response.json())
     .then((users) => {
-
+        console.log(users);
     })
 
 // 1.
